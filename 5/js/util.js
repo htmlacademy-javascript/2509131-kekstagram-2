01095c771df6934c4530a420d081ebfedbfeca47
Keks <keks@htmlacademy.ru>
2025-02-26 15:59:19 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/util.js
@@ -0,0 +1,33 @@
+function getRandomInteger (min, max) {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+}
+
+function getNumberInOrder () {
+  let lastGeneratedId = 0;
+
+  return function () {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+}
+
+function getRandomUniqIntegerNumber () {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = Math.floor(Math.random() * 1000);
+    while (previousValues.includes(currentValue)) {
+      currentValue = Math.floor(Math.random() * 1000);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+export {getRandomInteger, getNumberInOrder, getRandomArrayElement, getRandomUniqIntegerNumber};
