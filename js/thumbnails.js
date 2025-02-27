6fc5d55993ab13ba7f62e55f06ef1cd352469de4
Екатерина Машкина <51336273+Katerina2391@users.noreply.github.com>
2025-02-27 14:23:12 +0300

    Добавляет функции

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,25 +1,36 @@
-import {createMockPhotos} from './data.js';
+import {mockPhotos} from './data.js';
 
 const templatePicture = document.querySelector('#picture').content.querySelector('.picture');
-
 const picturesContainer = document.querySelector('.pictures');
-
 const documentFragment = document.createDocumentFragment();
 
-const mockPhotos = createMockPhotos();
+function fillPictureTemplate (picture) {
+  const thumbnail = templatePicture.cloneNode(true);
+  const thumbnailImg = thumbnail.querySelector('.picture__img');
+  const thumbnailComments = thumbnail.querySelector('.picture__comments');
+  const pictureLikes = thumbnail.querySelector('.picture__likes');
 
-mockPhotos.forEach(({url, description, likes, comments}) => {
-  const picture = templatePicture.cloneNode(true);
-  const pictureImg = picture.querySelector('.picture__img');
-  const pictureComments = picture.querySelector('.picture__comments');
-  const pictureLikes = picture.querySelector('.picture__likes');
+  thumbnailImg.src = picture.url;
+  thumbnailImg.alt = picture.description;
+  thumbnailComments.textContent = picture.comments.length;
+  pictureLikes.textContent = picture.likes;
 
-  pictureImg.src = url;
-  pictureImg.alt = description;
-  pictureComments.textContent = comments.length;
-  pictureLikes.textContent = likes;
+  return thumbnail;
+}
 
-  documentFragment.appendChild(picture);
-});
+function fillDocumentFragment (photos) {
+  photos.forEach((photo) => {
+    documentFragment.append(fillPictureTemplate(photo));
+  });
 
-picturesContainer.appendChild(documentFragment);
+  return documentFragment;
+}
+
+function renderPhotoCards (container, photosArray) {
+  container.append(fillDocumentFragment(photosArray));
+
+  return container;
+}
+
+renderPhotoCards(picturesContainer, mockPhotos);
+
