410c23fb370717b577579cb006b12f65ad8793bf
Keks <keks@htmlacademy.ru>
2025-04-03 18:49:52 +0300

    :heavy_check_mark: Сборка #11

--- 11/js/filter-buttons.js
+++ 11/js/filter-buttons.js
@@ -1,46 +1,56 @@
+import { renderPhotoCards } from './thumbnails.js';
+import { getRandomUniquePhotos } from './util.js';
+import { debounce } from './util.js';
+
 const imgFilters = document.querySelector('.img-filters');
-const filterDefaultButton = document.querySelector('#filter-default');
-const filterRandomButton = document.querySelector('#filter-random');
-const filterDiscussedButton = document.querySelector('#filter-discussed');
+const RANDOM_PHOTOS_NUMBER = 10;
+const RERENDER_DELAY = 500;
+let currentFilter = 'filter-default';
+let photosData = [];
 
-const removeActiveClass = () => {
-  const activeFilterButton = document.querySelector('.img-filters__button--active');
-  activeFilterButton.classList.remove('img-filters__button--active');
+const FILTERS = {
+  DEFAULT: 'filter-default',
+  RANDOM: 'filter-random',
+  DISCUSSED: 'filter-discussed'
 };
 
-const addActiveClass = (button) => {
-  button.classList.add('img-filters__button--active');
-};
-
-export const compareCommentsNumbers = (photoA, photoB) => {
+const compareCommentsNumbers = (photoA, photoB) => {
   const photoAComments = photoA.comments.length;
   const photoBComments = photoB.comments.length;
   return photoBComments - photoAComments;
 };
 
-export const setFilterDefaultButtonClick = (cb) => {
-  filterDefaultButton.addEventListener('click', () => {
-    removeActiveClass();
-    addActiveClass(filterDefaultButton);
-    cb();
-  });
-};
+function getFilteredPhotos (filterType) {
+  switch (filterType) {
+    case FILTERS.RANDOM:
+      return getRandomUniquePhotos(photosData, RANDOM_PHOTOS_NUMBER);
+    case FILTERS.DISCUSSED:
+      return photosData.slice().sort(compareCommentsNumbers);
+    case FILTERS.DEFAULT:
+    default:
+      return photosData;
+  }
+}
 
-export const setFilterRandomButtonClick = (cb) => {
-  filterRandomButton.addEventListener('click', () => {
-    removeActiveClass();
-    addActiveClass(filterRandomButton);
-    cb();
-  });
-};
+function onImgFiltersClick (evt) {
+  const clickedButton = evt.target.closest('.img-filters__button');
 
-export const setFilterDiscussedButtonClick = (cb) => {
-  filterDiscussedButton.addEventListener('click', () => {
-    removeActiveClass();
-    addActiveClass(filterDiscussedButton);
-    cb();
-  });
-};
+  if (!clickedButton || clickedButton.id === currentFilter) {
+    return;
+  }
+
+  const filteredPhotos = getFilteredPhotos(clickedButton.id);
+  currentFilter = clickedButton.id;
+  document.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+  clickedButton.classList.add('img-filters__button--active');
+
+  renderPhotoCards(filteredPhotos);
+}
+
+export function initFilters (photos) {
+  photosData = photos;
+  imgFilters.addEventListener('click', debounce(onImgFiltersClick, RERENDER_DELAY));
+}
 
 export const activateFilterButtons = () => imgFilters.classList.remove('img-filters--inactive');
 
--- 11/js/galery.js
+++ 11/js/galery.js
@@ -2,34 +2,15 @@ import { renderPhotoCards } from './thumbnails.js';
 import { fillPictures, openBigPicture } from './big-picture.js';
 import { showLoadingDataError } from './errors.js';
 import { getData } from './api.js';
-import { getRandomUniquePhotos, debounce } from './util.js';
-import { setFilterDefaultButtonClick, setFilterRandomButtonClick, setFilterDiscussedButtonClick, activateFilterButtons, compareCommentsNumbers } from './filter-buttons.js';
+import { activateFilterButtons, initFilters } from './filter-buttons.js';
 
 const picturesContainer = document.querySelector('.pictures');
-const RANDOM_PHOTOS_NUMBER = 10;
-const RERENDER_DELAY = 500;
 
 getData()
   .then((photos) => {
     fillPictures(photos);
     renderPhotoCards(photos);
-
-    setFilterDefaultButtonClick(debounce(
-      () => {
-        renderPhotoCards(photos);
-      }, RERENDER_DELAY));
-
-    setFilterRandomButtonClick(debounce(
-      () => {
-        const randomPhotos = getRandomUniquePhotos(photos, RANDOM_PHOTOS_NUMBER);
-        renderPhotoCards(randomPhotos);
-      }, RERENDER_DELAY));
-
-    setFilterDiscussedButtonClick(debounce(
-      () => {
-        const discussedPhotos = photos.slice().sort(compareCommentsNumbers);
-        renderPhotoCards(discussedPhotos);
-      }, RERENDER_DELAY));
+    initFilters(photos);
   }
   )
   .then(activateFilterButtons())
